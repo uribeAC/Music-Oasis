41bcd7cfa62e1bdeba764d1653773af6e5cb8e66
Ivet Lopez <ivet19@live.com>
2025-03-11 11:38:07 +0100

    Modify variables names and properties type of values

---
+++ src/disks/types.ts
@@ -0,0 +1,10 @@
+export type Record = {
+  id: number;
+  name: string;
+  artist: string;
+  coverUrl: string;
+  type: "vinyl" | "digital";
+  price: number;
+  originalPrice: number;
+  isDiskOfTheMonth: boolean;
+};
