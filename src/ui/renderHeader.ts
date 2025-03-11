4299648f67a370db3067f03413ad39d7ea438489
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-11 18:39:16 +0100

    Refine header first version

--- src/ui/renderHeader.ts
+++ src/ui/renderHeader.ts
@@ -1,11 +1,18 @@
 export const renderHeader = () => {
   const header = document.createElement("header");
-  header.classList.add("header-top");
+  header.classList.add("main-header");
 
   header.innerHTML = `
-    <button type="button">Caca</button>
-    <img src="./multimedia/icons/web-icon.svg" alt="Music Oasis logo">
-    <button type="button">Caca</button>
+    <button class="search" id="search-button" type="button">
+      <span>SEARCH</span>
+    </button>
+    <div class="logo-container">
+      <img class="logo-container__logo" src="./multimedia/icons/web-icon.svg" alt="Music Oasis logo">
+      <h1>MUSIC OASIS</h1>
+    </div>
+    <button class="cart" id="cart-button" type="button">
+      <span>CART</span>
+    </button>
   `;
   document.body.prepend(header);
 };
