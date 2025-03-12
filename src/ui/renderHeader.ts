cc16dfe2c1f8a92f9f830949e0d92e01ad3a31c1
Ivet Lopez <ivet19@live.com>
2025-03-12 12:43:54 +0100

    Add favicon

--- src/ui/renderHeader.ts
+++ src/ui/renderHeader.ts
@@ -7,7 +7,7 @@ export const renderHeader = () => {
       <span>SEARCH</span>
     </button>
     <div class="logo-container">
-      <img class="logo-container__logo" src="./multimedia/icons/web-icon.svg" alt="Music Oasis logo">
+      <img class="logo-container__logo" src="/multimedia/icons/web-icon.svg" alt="Music Oasis logo">
       <h1>MUSIC OASIS</h1>
     </div>
     <button class="cart" id="cart-button" type="button">
