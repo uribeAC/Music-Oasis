1101a4343854f86841c740aedb75da03557043df
Ivet Lopez <ivet19@live.com>
2025-03-12 20:47:15 +0100

    Fix main class header elements at html and styles.

--- src/ui/renderHeader.ts
+++ src/ui/renderHeader.ts
@@ -3,16 +3,10 @@ export const renderHeader = () => {
   header.classList.add("main-header");
 
   header.innerHTML = `
-    <button class="search" id="search-button" type="button">
-      <span>SEARCH</span>
-    </button>
     <div class="logo-container">
       <img class="logo-container__logo" src="/multimedia/icons/web-icon.svg" alt="Music Oasis logo">
       <h1>MUSIC OASIS</h1>
     </div>
-    <button class="cart" id="cart-button" type="button">
-      <span>CART</span>
-    </button>
   `;
   document.body.prepend(header);
 };
