df8e3fa6ffc9917f555fc145b0b434c17ec034ec
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-13 03:17:45 +0100

    Add record prices rendering to renderRecordCard

--- src/ui/renderRecordCard.ts
+++ src/ui/renderRecordCard.ts
@@ -12,6 +12,38 @@ export const renderRecordCard = (
     ".record__name"
   ) as HTMLHeadingElement;
 
+  const recordPrices = recordCard.querySelector(
+    ".record__prices"
+  ) as HTMLDivElement;
+
+  if (record.price === record.originalPrice) {
+    const recordPrice = document.createElement("span");
+
+    recordPrice.classList.add("record__price");
+    recordPrice.textContent = `$${record.originalPrice}`;
+
+    recordPrices.appendChild(recordPrice);
+  } else {
+    const recordOriginalPrice = document.createElement("span");
+    const strikethroughElement = document.createElement("s");
+
+    recordOriginalPrice.classList.add(
+      "record__price",
+      "record__price--original"
+    );
+
+    strikethroughElement.textContent = `$${record.originalPrice}`;
+    recordOriginalPrice.appendChild(strikethroughElement);
+
+    const recordPrice = document.createElement("span");
+
+    recordPrice.classList.add("record__price", "record__price--discounted");
+    recordPrice.textContent = `$${record.price}`;
+
+    recordPrices.appendChild(recordPrice);
+    recordPrices.appendChild(recordOriginalPrice);
+  }
+
   recordCover.src = record.coverUrl;
   recordName.textContent = `${record.name} (${record.artist})`;
 };
