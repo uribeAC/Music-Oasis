053a45f23dfb9f6b0437567df02bcd1723ee8c23
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-13 04:26:02 +0100

    Abstract renderRecordCard implementation into separate functions

--- src/ui/renderRecordCard.ts
+++ src/ui/renderRecordCard.ts
@@ -1,29 +1,27 @@
 import { Record } from "../records/types";
 
-export const renderRecordCard = (
-  record: Record,
-  recordCard: HTMLElement
-): void => {
+const renderCover = (record: Record, recordCard: HTMLElement): void => {
   const recordCover = recordCard.querySelector(
     ".record__cover"
   ) as HTMLImageElement;
 
+  recordCover.src = record.coverUrl;
+};
+
+const renderName = (record: Record, recordCard: HTMLElement): void => {
   const recordName = recordCard.querySelector(
     ".record__name"
   ) as HTMLHeadingElement;
 
+  recordName.textContent = `${record.name} (${record.artist})`;
+};
+
+const renderPrices = (record: Record, recordCard: HTMLElement): void => {
   const recordPrices = recordCard.querySelector(
     ".record__prices"
   ) as HTMLDivElement;
 
-  if (record.price === record.originalPrice) {
-    const recordPrice = document.createElement("span");
-
-    recordPrice.classList.add("record__price");
-    recordPrice.textContent = `$${record.originalPrice}`;
-
-    recordPrices.appendChild(recordPrice);
-  } else {
+  if (record.price !== record.originalPrice) {
     const recordOriginalPrice = document.createElement("span");
     const strikethroughElement = document.createElement("s");
 
@@ -31,22 +29,29 @@ export const renderRecordCard = (
       "record__price",
       "record__price--original"
     );
-
     strikethroughElement.textContent = `$${record.originalPrice}`;
+
     recordOriginalPrice.appendChild(strikethroughElement);
 
     const recordPrice = document.createElement("span");
-
     recordPrice.classList.add("record__price", "record__price--discounted");
     recordPrice.textContent = `$${record.price}`;
 
     recordPrices.appendChild(recordPrice);
     recordPrices.appendChild(recordOriginalPrice);
+
+    return;
   }
 
-  recordCover.src = record.coverUrl;
-  recordName.textContent = `${record.name} (${record.artist})`;
+  const recordPrice = document.createElement("span");
 
+  recordPrice.classList.add("record__price");
+  recordPrice.textContent = `$${record.originalPrice}`;
+
+  recordPrices.appendChild(recordPrice);
+};
+
+const renderDiscount = (record: Record, recordCard: HTMLElement): void => {
   const recordTags = recordCard.querySelector(
     ".record__tags"
   ) as HTMLDivElement;
@@ -57,4 +62,14 @@ export const renderRecordCard = (
 
     recordTags.prepend(recordTag);
   }
+};
+
+export const renderRecordCard = (
+  record: Record,
+  recordCard: HTMLElement
+): void => {
+  renderCover(record, recordCard);
+  renderName(record, recordCard);
+  renderPrices(record, recordCard);
+  renderDiscount(record, recordCard);
 };
