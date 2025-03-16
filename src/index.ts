d3aeced9e9f25f88a02adc69d58afe2fd8a5914d
Ivet Lopez <ivet19@live.com>
2025-03-16 12:26:19 +0100

    Add functionality to filters depending of the record type

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,24 @@ import { records } from "./records/data.js";
 import { renderHeader } from "./ui/renderHeader.js";
 import { renderRecordsList } from "./ui/renderRecordsList.js";
 
+const filterOptions = document.querySelectorAll(".filter-button");
+
+filterOptions.forEach((filterOption) => {
+  const thisFilterOption = filterOption as HTMLButtonElement;
+
+  thisFilterOption.addEventListener("click", () => {
+    if (thisFilterOption.value === "all") {
+      renderRecordsList(records);
+      return;
+    }
+
+    const filteredRecords = records.filter(
+      (record) => record.type === thisFilterOption.value
+    );
+
+    renderRecordsList(filteredRecords);
+  });
+});
+
 renderHeader();
 renderRecordsList(records);
