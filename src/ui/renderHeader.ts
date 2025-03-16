940544896ac3fe3d043edacf4d96d782a50d2fb8
Ivet Lopez <ivet19@live.com>
2025-03-16 18:44:36 +0100

    Fix aria-hidden state replacing : for =

--- src/ui/renderHeader.ts
+++ src/ui/renderHeader.ts
@@ -4,16 +4,16 @@ export const renderHeader = () => {
 
   header.innerHTML = `
     <button class= "header-button header-button__search" type="button" aria-label="search">
-      <img class="button-image button-image__search" src="/images/icons/search.svg" alt="red lens icon" aria-hidden:"true">
-      <span class="button-name" aria-hidden:"true">search</span>
+      <img class="button-image button-image__search" src="/images/icons/search.svg" alt="red lens icon" aria-hidden="true">
+      <span class="button-name" aria-hidden="true">search</span>
     </button>
     <div class="logo-container">
       <img class=" logo" src="/images/icons/logo.svg" alt="Music Oasis logo">
       <span class="shop-name">music oasis</span>
     </div>
     <button class="header-button header-button__cart" type="button" aria-label="go to your cart">
-      <img class="button-image button-image__cart" src="/images/icons/cart.svg" alt="red shopping cart icon" aria-hidden:"true">
-      <span class="button-name" aria-hidden:"true">cart</span>
+      <img class="button-image button-image__cart" src="/images/icons/cart.svg" alt="red shopping cart icon" aria-hidden="true">
+      <span class="button-name" aria-hidden="true">cart</span>
     </button>
   `;
   document.body.prepend(header);
