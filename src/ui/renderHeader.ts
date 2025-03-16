71a590f6ea1743588b8a1061925e5103ab65a8d6
Ivet Lopez <ivet19@live.com>
2025-03-16 17:23:31 +0100

    Convert span text to uppercase from styles sheet

--- src/ui/renderHeader.ts
+++ src/ui/renderHeader.ts
@@ -5,15 +5,15 @@ export const renderHeader = () => {
   header.innerHTML = `
     <button class= "header-button header-button__search" type="button">
       <img class="button-image button-image__search" src="/images/icons/search.svg">
-      <span class="button-name">SEARCH</span>
+      <span class="button-name">search</span>
     </button>
     <div class="logo-container">
       <img class=" logo" src="/images/icons/logo.svg" alt="Music Oasis logo">
-      <span class="shop-name">MUSIC OASIS</span>
+      <span class="shop-name">music oasis</span>
     </div>
     <button class="header-button header-button__cart" type="button">
       <img class="button-image button-image__cart" src="/images/icons/cart.svg">
-      <span class="button-name">CART</span>
+      <span class="button-name">cart</span>
     </button>
   `;
   document.body.prepend(header);
